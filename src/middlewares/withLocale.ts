0fd781a0eacb9ddfac016f0a0d768b35e0b007f1
dmytro-chushko <dmytro.chushko@gmail.com>
2025-02-25 22:18:09 +0200

    Add cookie to the lang feature

--- src/middlewares/withLocale.ts
+++ src/middlewares/withLocale.ts
@@ -1,10 +1,15 @@
 import { NextFetchEvent, NextRequest, NextResponse } from 'next/server';
 import { MiddlewareFactory } from './types';
-
-const locales = ['en', 'ua'];
+import { langs } from '@/lib/utils/getDictionary';
 
 // Get the preferred locale, similar to the above or using a library
 const getLocale = (request: NextRequest): string => {
+  const cookieLocale = request.cookies.get('NEXT_LOCALE')?.value;
+
+  if (cookieLocale && langs.includes(cookieLocale)) {
+    return cookieLocale;
+  }
+
   const acceptLanguage = request.headers.get('accept-language');
 
   if (acceptLanguage) {
@@ -12,7 +17,7 @@ const getLocale = (request: NextRequest): string => {
       .split(',')
       .map((lang) => lang.split(';')[0].trim());
 
-    const matchedLocale = browserLocales.find((lang) => locales.includes(lang));
+    const matchedLocale = browserLocales.find((lang) => langs.includes(lang));
 
     if (matchedLocale) {
       return matchedLocale;
@@ -25,9 +30,8 @@ const getLocale = (request: NextRequest): string => {
 export const withLocale: MiddlewareFactory = (next) => {
   return async (request: NextRequest, _next: NextFetchEvent) => {
     const { pathname } = request.nextUrl;
-    const pathnameHasLocale = locales.some(
-      (locale) =>
-        pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    const pathnameHasLocale = langs.some(
+      (lang) => pathname.startsWith(`/${lang}/`) || pathname === `/${lang}`
     );
 
     if (pathnameHasLocale) return next(request, _next);
