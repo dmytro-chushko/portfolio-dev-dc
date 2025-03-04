9b29dce13b0821565d4f7e47ed230b840b93f7fc
dmytro-chushko <dmytro.chushko@gmail.com>
2025-03-04 02:05:54 +0200

    Add mobile menu

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,4 +1,5 @@
 import NavLink from '@/components/ui/NavLink/NavLink';
+import './header.css';
 
 type HeaderProps = {
   linkLabels: Record<string, string>;
@@ -7,10 +8,25 @@ type HeaderProps = {
 const Header = ({ linkLabels }: HeaderProps) => {
   return (
     <>
-      <div className="flex">
-        {Object.keys(linkLabels).map((link) => (
-          <NavLink key={link} label={linkLabels[link]} href={`#${link}`} />
-        ))}
+      <input
+        type="checkbox"
+        id="toggle-menu-button"
+        className="opacity-0 w-6 h-6 absolute top-6 right-6 z-50 md:hidden"
+      />
+      <div className="w-6 h-6 absolute top-6 right-6 z-40 md:hidden">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div
+        id="toggle-menu"
+        className="fixed top-0 left-0 right-0 bottom-0 flex justify-center items-center bg-background opacity-0 z-30 transition-opacity duration-500 pointer-events-none md:static"
+      >
+        <ul className="flex flex-col md:flex-row md:justify-between md:p-6">
+          {Object.keys(linkLabels).map((link) => (
+            <NavLink key={link} label={linkLabels[link]} href={`#${link}`} />
+          ))}
+        </ul>
       </div>
     </>
   );
