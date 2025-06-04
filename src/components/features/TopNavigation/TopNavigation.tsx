bec5d827065236e76492246934c3285b206c5290
dmytro-chushko <dmytro.chushko@gmail.com>
2025-06-05 00:32:53 +0300

    Add auth to the dashboard nav item

--- src/components/features/TopNavigation/TopNavigation.tsx
+++ src/components/features/TopNavigation/TopNavigation.tsx
@@ -1,18 +1,38 @@
+'use client';
+
+import { useUser } from '@auth0/nextjs-auth0';
+import clsx from 'clsx';
 import { Children, ReactNode } from 'react';
 
 import NavLink from '@/components/ui/NavLink/NavLink';
+import { Dictionary } from '@/lib/utils/getDictionary';
+
+type LinkLabelsType =
+  | Dictionary['nav']['links']
+  | Dictionary['nav']['dashboard'];
+
+type UniLinkLabelsType = Dictionary['nav']['links'] &
+  Dictionary['nav']['dashboard'];
+
+type LinksLabelItemType =
+  | keyof Dictionary['nav']['links']
+  | keyof Dictionary['nav']['dashboard'];
 
 type TopNavigationProps = {
-  linkLabels: Record<string, string>;
+  linkLabels: LinkLabelsType;
   linkPrefix?: string;
   children?: ReactNode;
 };
+
+const PROTECTED_ITEMS: LinksLabelItemType[] = ['dc-dashboard'];
 
 const TopNavigation = ({
   linkLabels,
   linkPrefix,
   children,
 }: TopNavigationProps) => {
+  const { user } = useUser();
+
   return (
     <nav>
       <ul className="flex flex-col gap-2 lg:flex-row lg:justify-between lg:gap-6">
@@ -20,9 +40,18 @@ const TopNavigation = ({
           <li key={i}>{child}</li>
         ))}
         {Object.keys(linkLabels).map((link) => (
-          <li key={link}>
+          <li
+            key={link}
+            className={clsx(
+              PROTECTED_ITEMS.includes(link as LinksLabelItemType) &&
+                !user &&
+                'hidden'
+            )}
+          >
             <NavLink
-              label={linkLabels[link]}
+              label={
+                (linkLabels as UniLinkLabelsType)[link as LinksLabelItemType]
+              }
               href={`${linkPrefix || ''}${link}`}
             />
           </li>
