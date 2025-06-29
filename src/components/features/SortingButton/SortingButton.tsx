f5601aad640b3f7a525a24ad53b6528cf6af3746
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-06-29 23:45:08 +0300

    Change label

--- src/components/features/SortingButton/SortingButton.tsx
+++ src/components/features/SortingButton/SortingButton.tsx
@@ -3,7 +3,7 @@
 import Button from '@/components/ui/Button/Button';
 
 const SortingButton = ({ onClick }: { onClick: () => void }) => {
-  return <Button onClick={onClick}>Sorting change</Button>;
+  return <Button onClick={onClick}>Sorting</Button>;
 };
 
 export default SortingButton;
