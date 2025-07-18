5590e589981b9cce697f991752b0913d3c89bf93
Dmytro Chushko <dmytro.chushko@gmail.com>
2025-07-18 08:18:49 +0300

    Change

--- src/components/features/SortingButton/SortingButton.tsx
+++ src/components/features/SortingButton/SortingButton.tsx
@@ -3,7 +3,7 @@
 import Button from '@/components/ui/Button/Button';
 
 const SortingButton = ({ onClick }: { onClick: () => void }) => {
-  return <Button onClick={onClick}>Sorting button</Button>;
+  return <Button onClick={onClick}>Sorting</Button>;
 };
 
 export default SortingButton;
