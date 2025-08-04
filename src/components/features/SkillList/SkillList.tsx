2b1292a9cd7b77674718b070a92061e44de78918
dmytro-chushko <dmytro.chushko@gmail.com>
2025-08-04 23:47:54 +0300

    Add category property to the skills mock

--- src/components/features/SkillList/SkillList.tsx
+++ src/components/features/SkillList/SkillList.tsx
@@ -7,6 +7,7 @@ type SkillListProps = {
   skills: {
     techName: string;
     title: string;
+    category: string;
     description: string;
     estimate: number;
   }[];
@@ -16,18 +17,20 @@ const SkillList = ({ skills }: SkillListProps) => {
   return (
     <ul className="flex items-center gap-y-4 flex-wrap">
       {skills.length > 0 &&
-        skills.map(({ techName, title, description }) => (
-          <li
-            key={techName}
-            className="flex items-center gap-2 sm:w-1/2 md:w-1/3 lg:w-1/4"
-          >
-            <TechIconFactory techName={techName} size={64} />
-            <div>
-              <Paragraph accent>{title}</Paragraph>
-              <Paragraph>{description}</Paragraph>
-            </div>
-          </li>
-        ))}
+        skills
+          .filter(({ category }) => category === 'primary')
+          .map(({ techName, title, description }) => (
+            <li
+              key={techName}
+              className="flex items-center gap-2 sm:w-1/2 md:w-1/3 lg:w-1/4"
+            >
+              <TechIconFactory techName={techName} size={64} />
+              <div>
+                <Paragraph accent>{title}</Paragraph>
+                <Paragraph>{description}</Paragraph>
+              </div>
+            </li>
+          ))}
     </ul>
   );
 };
