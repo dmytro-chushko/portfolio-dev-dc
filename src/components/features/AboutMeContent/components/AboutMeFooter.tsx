d6e00118af8ce653ef7542db9e7a3c68c7266d4f
dmytro-chushko <dmytro.chushko@gmail.com>
2025-09-01 23:56:42 +0300

    Add about me footer

---
+++ src/components/features/AboutMeContent/components/AboutMeFooter.tsx
@@ -0,0 +1,25 @@
+import Title from '@/components/typography/Title/Title';
+import StyledImage from '@/components/ui/StyledImage/StyledImage';
+
+type AboutMeFooterProps = {
+  footerTitle: string;
+  footerImg: string;
+};
+
+export default async function AboutMeFooter({
+  footerTitle,
+  footerImg,
+}: AboutMeFooterProps) {
+  return (
+    <div className="h-full md:flex md:items-center md:gap-6 md:justify-around">
+      <Title header="h3" copy={footerTitle} />
+      <StyledImage
+        className="md:w-1/2"
+        imgSrc={footerImg}
+        altText="title Dmytro Chushko"
+        width={925}
+        height={648}
+      />
+    </div>
+  );
+}
